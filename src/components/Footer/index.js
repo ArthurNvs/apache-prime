d318587cd72f922a7f91716f289c9c03faa9db55
theAapache <arthurnvsm@gmail.com>
2020-07-28 01:06:24 -0300

    Alterações na em App.js e Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="ApachePrime logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Projeto de Arthur Neves (theApache) feito com chá, chocolate e amor (: <br />
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          #Imersão React da Alura
         </a>
       </p>
     </FooterBase>
